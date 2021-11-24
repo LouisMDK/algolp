6ccaafb845f201eb77f8dfb1aa346c27f32c089d
Louis PAINTER <E211731S@univ-nantes.fr>
2021-11-24 15:50:14 +0100

    Add double linked list

--- structures/double_linked_list.go
+++ structures/double_linked_list.go
@@ -1,5 +1,40 @@
 package algolp
 
-type DoubleLinkedList struct {
+import "fmt"
 
+type DoubleLinkedList struct {
+	v int
+	next *DoubleLinkedList
+	prev *DoubleLinkedList
+}
+
+func (l DoubleLinkedList) Head() int {
+	return l.v
+}
+
+func (l DoubleLinkedList) Tail() DoubleLinkedList {
+	return *(l.next)
+}
+
+func GetEmptyDoubleList() DoubleLinkedList{
+	return DoubleLinkedList{next: nil, prev: nil}
+}
+
+func AppendDouble(val int, l DoubleLinkedList) DoubleLinkedList{
+	var new DoubleLinkedList = DoubleLinkedList{v: val, next: &l, prev: nil}
+	l.prev = &new
+	return new
+}
+
+func (l DoubleLinkedList) Affiche() string{
+	var s string
+	for !l.IsEmpty() {
+		s = fmt.Sprint(s, " ", l.Head())
+		l = l.Tail()
+	}
+	return s
+}
+
+func (l DoubleLinkedList) IsEmpty() bool{
+	return l.next == nil
 }
--- structures/double_linked_list_test.go
+++ structures/double_linked_list_test.go
@@ -1,1 +1,51 @@
 package algolp
+
+import (
+	"testing"
+)
+func TestDoubleVide(t *testing.T) {
+	var l DoubleLinkedList = GetEmptyDoubleList()
+	if !l.IsEmpty() {
+		t.Fail()
+	}
+}
+
+func TestDoubleListe(t *testing.T) {
+	var l DoubleLinkedList = GetEmptyDoubleList()
+	var d DoubleLinkedList = DoubleLinkedList{v: 2, next: &l, prev: nil}
+
+	if d.Tail() != l || d.Head() != 2 || d.prev != nil{
+		t.Fail()
+	}
+}
+
+func TestDoubleListe2(t *testing.T) {
+	var l DoubleLinkedList = GetEmptyDoubleList()
+	var d DoubleLinkedList = DoubleLinkedList{v: 1, next: &l, prev: nil}
+	l.prev = &d
+	var f DoubleLinkedList = DoubleLinkedList{v: 2, next: &d, prev: nil}
+	d.prev = &f
+	if f.Head() != 2 || f.prev != nil || f.next != &d || f.next.Head() != 1 || !f.next.next.IsEmpty(){
+		t.Fail()
+	}
+}
+
+func TestDoubleListAppend(t *testing.T) {
+	var e DoubleLinkedList = GetEmptyDoubleList()
+	var f DoubleLinkedList = AppendDouble(2, e)
+	var g DoubleLinkedList = AppendDouble(3, f)
+	var h DoubleLinkedList = AppendDouble(4, g)
+
+	if h.Head() != 4 || h.Tail().Head() != 3 || h.prev != nil{
+		t.Fail()
+	}
+	if h.next.Head() != 3 || h.next.prev.Head() != 4 {
+		t.Fail()
+	}
+	if h.next.next.Head() != 2 || h.next.next.prev.Head() != 3 {
+		t.Fail()
+	}
+	if !h.next.next.next.IsEmpty() || h.next.next.next.prev.Head() != 2{
+		t.Fail()
+	}
+}
